cd74ed79717bd23b11cf0808e8f96dc908d5df81
Manuel Sousa <mlrcbsousa@gmail.com>
2022-05-02 20:15:40 +0200

    Use tabs on the example cubfile.

--- src/game.c
+++ src/game.c
@@ -6,7 +6,7 @@
 /*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/01 18:38:35 by msousa            #+#    #+#             */
-/*   Updated: 2022/05/02 20:00:35 by msousa           ###   ########.fr       */
+/*   Updated: 2022/05/02 20:11:40 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,37 @@ t_bool	are_walls_set(t_app *self)
 	return (FALSE);
 }
 
-int	set_game_map()
+int	set_game_map(int fd, t_app *self)
+{
+	int		status;
+	char	*line;
+
+	status = 1;
+	line = NULL;
+	while (!are_walls_set(self))
+	{
+		status = get_next_line(fd, &line);
+		if (line)
+			printf("%s\n", line);
+
+		// TODO: game elements come first
+		if (valid_game_element(line))
+			set_game_element(line, self);
+
+		// TODO: map comes last
+		// so must have all elements before starting map parsing
+
+		free(line);
+	}
+	if (status < 0)
+		return (EXIT_FAILURE);
+	return (status);
+}
+
+int	set_game_walls(int fd, t_app *self)
 {}
 
-int	set_game_walls()
-{}
-
-int	set_game_colors()
+int	set_game_colors(int fd, t_app *self)
 {}
 
 // ASSUMES already validated
@@ -51,9 +75,11 @@ int	set_game_elements(int fd, t_app *self)
 
 	status = 1;
 	line = NULL;
-	while (!are_walls_set(self))
+	while (status > 0)
 	{
 		status = get_next_line(fd, &line);
+		if (line)
+			printf("%s\n", line);
 		// not needed if validated
 		// if (status > 0)
 		// {
@@ -61,12 +87,9 @@ int	set_game_elements(int fd, t_app *self)
 		// 		free(line);
 		// 	break ;
 		// }
-		if (line)
-			printf("%s\n", line);
 
 		// TODO: game elements come first
-		if (valid_game_element(line))
-			set_game_element(line, self);
+		set_game_element(line, self);
 
 		// TODO: map comes last
 		// so must have all elements before starting map parsing
