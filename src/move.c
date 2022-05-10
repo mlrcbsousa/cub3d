831563e5e940f4666aa7fc511ddc7079afd4dcd0
Manuel Sousa <mlrcbsousa@gmail.com>
2022-05-10 23:21:02 +0100

    Put map upright.

--- src/move.c
+++ src/move.c
@@ -6,7 +6,7 @@
 /*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 00:13:00 by msousa            #+#    #+#             */
-/*   Updated: 2022/05/10 23:15:35 by msousa           ###   ########.fr       */
+/*   Updated: 2022/05/10 23:20:17 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,7 @@ static void	move_left(t_app *self)
 	int			angle;
 
 	player = self->player;
-	angle = trim(player->angle + PI / 2);
+	angle = trim(player->angle - PI / 2);
 
 	delta = point(cos(angle), sin(angle));
 	delta = point_multiply(MOVE, delta);
@@ -76,7 +76,7 @@ static void	move_right(t_app *self)
 	int			angle;
 
 	player = self->player;
-	angle = trim(player->angle - PI / 2);
+	angle = trim(player->angle + PI / 2);
 
 	delta = point(cos(angle), sin(angle));
 	delta = point_multiply(MOVE, delta);
--- src/player.c
+++ src/player.c
@@ -6,7 +6,7 @@
 /*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/08 17:06:33 by msousa            #+#    #+#             */
-/*   Updated: 2022/05/10 12:40:07 by msousa           ###   ########.fr       */
+/*   Updated: 2022/05/10 23:19:47 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ static double	get_initial_angle(char direction)
 {
 	printf("direction: %c\n", direction);
 	if (direction == MAP_NORTH)
-		return (PI / 2);
-	else if (direction == MAP_SOUTH)
 		return (3 * PI / 2);
+	else if (direction == MAP_SOUTH)
+		return (PI / 2);
 	else if (direction == MAP_EAST)
 		return (0);
 	else if (direction == MAP_WEST)
